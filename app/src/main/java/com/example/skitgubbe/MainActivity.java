9e7c4949d3d7bfde31d6fb798436958453d1e0b7
Partast1 <andersa1989@gmail.com>
2020-11-25 08:57:40 +0100

    Initial commit

---
+++ app/src/main/java/com/example/skitgubbe/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.skitgubbe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
